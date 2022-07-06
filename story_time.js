e0467ed75de4695387b3b8db1d5223166c95395a
hjkim0514 <97289374+hjkim0514@users.noreply.github.com>
2022-07-06 15:36:11 +0900

    Add files via upload

---
+++ story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to Jeoji!";
+let name = "Ko SeoHyun";
+let title = "best friend of Yehee and JSJ";
+let worldName = "Jeoji";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 28;
+let year = 4044;
+let century = 40;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponUsed = ["axe", "hammer", "arrow", "sharp stick"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let HasWeakWeapon = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " My name is " + name + ".");
+console.log("I'm the " + title);
+console.log("As the leader of " + worldName + ", I have various weapons.");
+console.log("Time works two times faster in my world.");
+console.log("For example, I'm " + age + " years old.");
+console.log("In my world, you are in year " + year +" the " + century+"th century."+"\n");
+console.log("Does anyone have any questions?");
+console.log("Me: Do you have weak weapon?");
+console.log("SeoHyun: "+ HasWeakWeapon+ ", my weakest weapon is " + weaponUsed[-1] +"."+"\n");
+console.log("You are free to explore my world and return to Earth after your tour is over!");
